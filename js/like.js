e8664c98917f78a3d7b74f390780b878601b1e08
carlos_roos44 <108555868+carlos-roos44@users.noreply.github.com>
2022-12-01 16:29:18 -0300

    Uploading

---
+++ js/like.js
@@ -0,0 +1,39 @@
+const dandoLike = document.querySelector('.curtir1');
+dandoLike.addEventListener('click', function() {
+  dandoLike.classList.toggle('curtido1');
+});
+
+const dandoLike2 = document.querySelector('.curtir2');
+dandoLike2.addEventListener('click', function() {
+  dandoLike2.classList.toggle('curtido2');
+});
+
+const dandoLike3= document.querySelector('.curtir3');
+dandoLike3.addEventListener('click', function() {
+  dandoLike3.classList.toggle('curtido3');
+});
+
+const dandoLike4 = document.querySelector('.curtir4');
+dandoLike4.addEventListener('click', function() {
+  dandoLike4.classList.toggle('curtido4');
+});
+
+const dandoLike5 = document.querySelector('.curtir5');
+dandoLike5.addEventListener('click', function() {
+  dandoLike5.classList.toggle('curtido5');
+});
+
+const dandoLike6 = document.querySelector('.curtir6');
+dandoLike6.addEventListener('click', function() {
+  dandoLike6.classList.toggle('curtido6');
+});
+
+const dandoLike7 = document.querySelector('.curtir7');
+dandoLike7.addEventListener('click', function() {
+  dandoLike7.classList.toggle('curtido7');
+});
+
+const dandoLike8 = document.querySelector('.curtir8');
+dandoLike8.addEventListener('click', function() {
+  dandoLike8.classList.toggle('curtido8');
+});
